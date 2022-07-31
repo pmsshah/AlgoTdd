69f2a1442af1e02872ea2e4be1210e8f117dacc7
Piyush Shah <pshah427@gatech.edu>
2022-07-31 12:29:44 -0400

    br [GRAPH_ALGO] all graph travel

--- src/main/java/com/example/graph/GraphBFS.java
+++ src/main/java/com/example/graph/GraphBFS.java
@@ -40,4 +40,35 @@ public class GraphBFS {
         }
         System.out.println(sb.toString());
     }
+
+    public void BFSAllGraph() {
+        int i=0;
+        boolean visited[] = new boolean[V];
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+        StringBuffer sb = new StringBuffer();
+        sb.append("BFS All Graph : ");
+        for(i=0; i<V; i++) {
+            Iterator<Integer> it = adj[i].listIterator();
+            while (it.hasNext()) {
+                int n = it.next();
+                if (!visited[n]) {
+                    visited[n] = true;
+                    queue.add(n);
+                }
+            }
+        }
+        while (queue.size() != 0) {
+            int s = queue.poll(); //deque the value
+            sb.append(s).append(" ");
+            Iterator<Integer> it = adj[s].listIterator();
+            while (it.hasNext()) {
+                int n = it.next();
+                if(!visited[n]) {
+                    visited[n]=true;
+                    queue.add(n);
+                }
+            }
+        }
+        System.out.println(sb.toString());
+    }
 }
--- src/test/java/com/example/graph/GraphBFSTest.java
+++ src/test/java/com/example/graph/GraphBFSTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GraphBFSTest {
@@ -17,16 +19,28 @@ class GraphBFSTest {
 
     @Test
     void BFS() {
-        GraphBFS g = new GraphBFS(4);
-        g.addEdge(0, 1);
-        g.addEdge(0, 2);
-        g.addEdge(1, 2);
-        g.addEdge(2, 0);
-        g.addEdge(2, 3);
-        g.addEdge(3, 3);
-        g.BFS(2);
-        g.BFS(0);
-        g.BFS(1);
-        g.BFS(3);
+        GraphBFS g = new GraphBFS(4); //adj = new LinkedList[v];
+        g.addEdge(0, 1);        //adj[0] --> 1
+        g.addEdge(0, 2);    //adj[0] --> 1, 2
+        g.addEdge(1, 2);    //adj[1] --> 2
+        g.addEdge(2, 0);        //adj[2] --> 0
+        g.addEdge(2, 3);    //adj[2] --> 0, 3
+        g.addEdge(3, 3);    //adj[3] --> 3
+        g.BFS(2);   //2031  add(2). pop(2)->(add 03TT). pop(0)->(add 312TTF). pop(3)->(add 3123TTFF)
+        g.BFS(0);   //0123
+        g.BFS(1);   //1203
+        g.BFS(3);   //3
+    }
+
+    @Test
+    void BFSAllGraphTest() {
+        GraphBFS g = new GraphBFS(4); //adj = new LinkedList[v];
+        g.addEdge(0, 1);        //adj[0] --> 1
+        g.addEdge(0, 2);    //adj[0] --> 1, 2
+        g.addEdge(1, 2);    //adj[1] --> 2
+        g.addEdge(2, 0);        //adj[2] --> 0
+        g.addEdge(2, 3);    //adj[2] --> 0, 3
+        g.addEdge(3, 3);    //adj[3] --> 3
+        g.BFSAllGraph();
     }
 }
